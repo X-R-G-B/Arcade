7eeabf437089557bf67ef07b8413fca5498e661c
KitetsuK <brice.desousa@epitech.eu>
2023-03-22 21:06:02 +0100

    Add updated ASprite implementation

--- src/Arcade/Graph/ASprite.hpp
+++ src/Arcade/Graph/ASprite.hpp
@@ -15,27 +15,31 @@ namespace Arcade {
             public:
                 ASprite(std::string &id);
                 ~ASprite();
-                const std::string &getId() const;
                 const std::string &getPath() const;
                 const TTYData &getTTYData() const;
                 const Arcade::Vector3f &getPos() const;
                 const Arcade::Vector3f &getSize() const;
                 const Color &getColor() const;
-                const Rect getRect() const;
+                const Rect &getRect() const;
                 const float getScale() const;
 
 
-                void setPath(const std::string &text);
+                void setPath(const std::string &path);
                 void setTTYData(const TTYData &ttydata);
-                void setPos(Arcade::Vector3f &pos);
-                void setSize(Arcade::Vector3f &size);
+                void setPos(Vector3f &pos);
+                void setSize(Vector3f &size);
                 void setRect(Rect &rect);
                 void setColor(Color &color);
                 void setScale(float scale);
             protected:
             private:
-                std::string _Id;
                 std::string _Path;
+                TTYData _TTYData;
+                Vector3f _Pos;
+                Vector3f _Size;
+                Rect _Rect;
+                Color _Color;
+                float _Scale;
         };
     }
 }
