9c038de6e85fe5abaa57fcc7096103bdcda72a3f
KitetsuK <brice.desousa@epitech.eu>
2023-03-23 01:23:16 +0100

    Modif AText to become Text class Add implementation of Text constructor Update archi

---
+++ src/Arcade/Graph/Text.hpp
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2023
+** Arcade
+** File description:
+** Text
+*/
+
+#pragma once
+
+#include "IText.hpp"
+#include "AComponent.hpp"
+
+namespace Arcade {
+    namespace Graph {
+        class Text : public IText, public Arcade::ECS::AComponent {
+            public:
+                Text(const std::string &id);
+                ~Text();
+
+                const std::string &getId() const final;
+                const std::string &getFontPath() const final;
+                const std::string &getText() const final;
+                const Arcade::Graph::Color &getForegroundColor() const final;
+                const Arcade::Graph::Color &getBackgroundColor() const final;
+                const Arcade::Vector2f &getPos() const final;
+                float getPoliceSize() const final;
+
+
+                void setFontPath(const std::string &path) final;
+                void setText(const std::string &text) final;
+                void setBackgroundColor(Arcade::Graph::Color &color) final;
+                void setForegroundColor(Arcade::Graph::Color &color) final;
+                void setPoliceSize(float size) final;
+                void setPos(Arcade::Vector2f &pos) final;
+
+            protected:
+            private:
+                std::string _Id;
+                std::string _FontPath;
+                std::string _Text;
+                float _PoliceSize;
+                Color _ForegroundColor;
+                Color _BackgroundColor;
+                Arcade::Vector2f _Pos;
+        };
+    }
+}
