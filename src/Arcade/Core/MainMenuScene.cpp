29fb8aa3d8c5260cf3dd932efc59a93e47122b4e
Kitetsu <brice.desousa@epitech.eu>
2023-03-27 17:10:28 +0200

    Add first implementation of main menu scene

---
+++ src/Arcade/Core/MainMenuScene.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** Arcade
+** File description:
+** MainMenuScene
+*/
+
+#include "MainMenuScene.hpp"
+
+bool MainMenuScene::init()
+{
+    return (true);
+}
+
+void MainMenuScene::close()
+{
+
+}
---
+++ src/Arcade/Core/MainMenuScene.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** Arcade
+** File description:
+** MainMenuScene
+*/
+
+#pragma once
+
+#include "AScene.hpp"
+
+class MainMenuScene : public Arcade::Game::AScene {
+    public:
+        bool init() final;
+        void close() final;
+    protected:
+    private:
+};
