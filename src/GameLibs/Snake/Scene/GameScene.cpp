479057ccfeb5f0e57da364c08cb4283343fcb033
Kitetsu <brice.desousa@epitech.eu>
2023-03-29 17:54:33 +0200

    Add scene for snake game

---
+++ src/GameLibs/Snake/Scene/GameScene.cpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2023
+** Arcade
+** File description:
+** GameScene
+*/
+
+#include <iostream>
+#include "GameScene.hpp"
+
+Snake::Scene::GameScene::GameScene(std::unique_ptr<Arcade::ECS::IEntityManager> enitityManager)
+    : Arcade::Game::AScene(std::move(enitityManager))
+{
+}
+
+bool Snake::Scene::GameScene::init()
+{
+    return (true);
+}
+
+void Snake::Scene::GameScene::close()
+{
+}
