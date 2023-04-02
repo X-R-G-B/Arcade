8adeb737001d5c616793acc24efb7b1a62ee1639
KitetsuK <brice.desousa@epitech.eu>
2023-04-02 02:04:30 +0200

    Modif name of the component to implement

--- src/GameLibs/Snake/System/EatSystem.cpp
+++ src/GameLibs/Snake/System/EatSystem.cpp
@@ -19,11 +19,12 @@ void Snake::System::EatSystem::run(float deltaTime,
                               Arcade::ECS::IEntityManager &currentEntityManager)
 {
     auto snakeEntity = currentEntityManager.getEntitiesById(SNAKE_ENTITY);
-    auto &snakeIncrComp = snakeEntity->getComponents(SNAKE_INCR_COMP);
+    auto &snakeIncrComp = snakeEntity->getComponents("snakegrow");
 
-    if (snakeIncrComp.type == Arcade::ECS::CompType::SPRITE) {
-        //if (eventManager.isEventTriggered(EATED).first) { The if condition will work when EATED will be defined
-            //TODO incr the data of snakeIncrComp + 1
-        //}
+    if (snakeIncrComp.type == Arcade::ECS::CompType::SNAKEGROW) {
+        if (eventManager.isEventTriggered(EATED).first) {
+            snakeIncrComp.grow += 1;
+            //TODO increment the score when implemented
+        }
     }
 }
