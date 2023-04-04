da020a6c34cdf53daeec6e2d1e4e463d82ca3c14
Xavier Mitault <xavier.mitault@epitech.eu>
2023-04-04 20:29:59 +0200

    Call function that actualy print

--- src/GraphicLibs/ncurses/TEXT.cpp
+++ src/GraphicLibs/ncurses/TEXT.cpp
@@ -18,7 +18,7 @@ static const short foregroundColor = 9;
 static const short backgroundColor = 10;
 static const short colorPair = 11;
 
-bool Ncurses::System::TextSystem::printText(std::shared_ptr<Arcade::Graph::Text> text)
+bool Ncurses::System::TextSystem::printText(const std::shared_ptr<Arcade::Graph::Text> text)
 {
     int x = DisplayModule::getXFromX1920(text->pos.x);
     std::cerr << "x: " << x << std::endl;
@@ -48,10 +48,11 @@ void Ncurses::System::TextSystem::run(float deltaTime, Arcade::ECS::IEventManage
 {
     auto texts = entityManager.getEntitiesByComponentType(Arcade::ECS::CompType::TEXT);
 
-    for (auto &text : *texts) {
-        auto textComponents = text->getComponents(Arcade::ECS::CompType::TEXT);
-        for (auto &textComponent : textComponents) {
-            auto text = std::static_pointer_cast<Arcade::Graph::Text>(textComponent);
+    for (const auto &entity : *texts) {
+        auto textComponents = entity->getComponents(Arcade::ECS::CompType::TEXT);
+        for (const auto &textComponent : textComponents) {
+            const auto text = std::static_pointer_cast<Arcade::Graph::Text>(textComponent);
+            this->printText(text);
         }
     }
 }
