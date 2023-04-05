a51c0f0d4503599084693388e5394deeeb386428
Xavier Mitault <xavier.mitault@epitech.eu>
2023-04-05 20:06:00 +0200

    Pos update for mainmenu

--- src/Arcade/Core/MainMenuScene.cpp
+++ src/Arcade/Core/MainMenuScene.cpp
@@ -26,9 +26,7 @@ bool Arcade::Core::MainMenuScene::init()
     Arcade::ECS::IEntity &gamesEntity = entityManager.createEntity("Games");
     Arcade::ECS::IEntity &graphicsEntity = entityManager.createEntity("Graphics");
     std::shared_ptr<Arcade::Graph::Text> text = nullptr;
-    Arcade::Vector3f compPos = {
-        10, 20, 0
-    };
+    Arcade::Vector3f compPos = {10, 20, 0};
 
     for (std::size_t i = 0; i != _gameLibs.size(); i++) {
         text = std::make_shared<Arcade::Graph::Text>(_gameLibs[i].first);
@@ -36,18 +34,18 @@ bool Arcade::Core::MainMenuScene::init()
         text->text = _gameLibs[i].first;
         text->textColor = {255, 0, 0, 255};
         text->pos = compPos;
-        compPos.x += 24;
+        compPos.y += 24;
         gamesEntity.addComponent(text);
     }
-    compPos.y += 20;
-    compPos.x = 10;
+    compPos.y = 10;
+    compPos.x = 1920.0 / 2.0;
     for (std::size_t i = 0; i != _graphicLibs.size(); i++) {
         text = std::make_shared<Arcade::Graph::Text>(_graphicLibs[i].first);
         text->fontPath = "./assets/Menu/Roboto-Thin.ttf";
         text->text = _graphicLibs[i].first;
         text->textColor = {255, 0, 0, 255};
         text->pos = compPos;
-        compPos.x += 24;
+        compPos.y += 24;
         graphicsEntity.addComponent(text);
     }
     return (true);
