3c5d35120f624de02456cf676f23f7aa73238cac
romain <romain.panno@epitech.eu>
2023-04-05 23:14:04 +0200

    Fix missing use of random method variable in addNibblerWall Fix wrong variable name Todelete -> ToDelete Fix wrong implementation of empty vector check : if (v.size() == 0) > if (v.empty())

--- src/GameLibs/Nibbler/Scene/GameScene.cpp
+++ src/GameLibs/Nibbler/Scene/GameScene.cpp
@@ -32,10 +32,10 @@ bool Nibbler::Scene::GameScene::checkWallCollision(Arcade::ECS::IEntity &nibbler
     if (pos.x < NIBBLER_PADDING_WINDOW_X || pos.x >= MAP_RIGHT || pos.y < NIBBLER_PADDING_WINDOW_Y || pos.y >= MAP_BOTTOM) {
         return true;
     }
-    if (walls.size() == 0) {
+    if (walls.empty()) {
         return false;
     }
-    for (auto tmp : walls) {
+    for (auto &tmp : walls) {
         auto wall = std::static_pointer_cast<Nibbler::Component::NibblerWallComponent>(tmp);
         if (std::find(idToIgnore.begin(), idToIgnore.end(), wall->id) != idToIgnore.end()) {
             continue;
@@ -83,10 +83,10 @@ void Nibbler::Scene::GameScene::addNibblerWallSquare(Arcade::ECS::IEntity &nibbl
     Arcade::Vector3f pos = src_pos;
     std::vector<std::string> toIgnore;
     std::string wallId;
-    int borderTodelete = generateRandomInt(3);
+    int borderToDelete = generateRandomInt(3);
 
     for (int i = 0; i < 4; i++) {
-        if (!checkWallCollision(nibblerWalls, pos, toIgnore) && i != borderTodelete) {
+        if (!checkWallCollision(nibblerWalls, pos, toIgnore) && i != borderToDelete) {
             wallId = std::string(NIBBLER_WALL_ID) + "_" + std::to_string(id) + "_nbr_" + std::to_string(i);
             nibblerWalls.addComponent(std::make_shared<Nibbler::Component::NibblerWallComponent>(wallId, pos));
             toIgnore.push_back(wallId);
@@ -115,7 +115,7 @@ bool Nibbler::Scene::GameScene::checkParcelCollision(Arcade::ECS::IEntity &nibbl
     if (pos.x < NIBBLER_PADDING_WINDOW_X || pos.x >= MAP_RIGHT || pos.y < NIBBLER_PADDING_WINDOW_Y || pos.y >= MAP_BOTTOM) {
         return true;
     }
-    for (auto tmp : walls) {
+    for (auto &tmp : walls) {
         auto wall = std::static_pointer_cast<Nibbler::Component::NibblerWallComponent>(tmp);
         auto wallPos = wall->pos;
         if (pos.x >= wallPos.x && pos.x < wallPos.x + PARCELL_SIZE
@@ -133,7 +133,7 @@ void Nibbler::Scene::GameScene::addNibblerWall(Arcade::ECS::IEntity &nibblerWall
     };
     auto funcNbr = generateRandomInt(addNibblerWallFuncs.size() - 1);
 
-    addNibblerWallFuncs[0](*this, nibblerWalls, pos, nbrOfWalls);
+    addNibblerWallFuncs[funcNbr](*this, nibblerWalls, pos, nbrOfWalls);
     this->addNibblerWallSquare(nibblerWalls, pos, nbrOfWalls);
 }
 
@@ -158,12 +158,13 @@ void Nibbler::Scene::GameScene::addNibblerMap()
     Arcade::ECS::IEntity &nibblerMap = EntityManager.createEntity(NIBBLER_MAP_ID);
     Arcade::ECS::IEntity &nibblerWalls = EntityManager.createEntity(NIBBLER_WALL_ID);
     std::vector<int> wallsPos;
-    int nibblerMapParcelNbr = 0;
 
+    // generate randoms positions of walls
     for (int i = 0; i < NBR_OF_WALLS; i++) {
         wallsPos.push_back(generateRandomInt(MAP_PARCELLS_X * MAP_PARCELLS_Y));
     }
-    for (int y = 0; y < MAP_PARCELLS_Y; y++, nibblerMapParcelNbr++) {
+    // generate walls
+    for (int y = 0, nibblerMapParcelNbr = 0; y < MAP_PARCELLS_Y; y++, nibblerMapParcelNbr++) {
         for (int x = 0; x < MAP_PARCELLS_X; x++, nibblerMapParcelNbr++) {
             if (std::find(wallsPos.begin(), wallsPos.end(), nibblerMapParcelNbr) != wallsPos.end()) {
                 addNibblerWall(nibblerWalls,
@@ -173,8 +174,8 @@ void Nibbler::Scene::GameScene::addNibblerMap()
             }
         }
     }
-    nibblerMapParcelNbr = 0;
-    for (int y = 0; y < MAP_PARCELLS_Y; y++, nibblerMapParcelNbr++) {
+    // generate map parcels
+    for (int y = 0, nibblerMapParcelNbr = 0; y < MAP_PARCELLS_Y; y++, nibblerMapParcelNbr++) {
         for (int x = 0; x < MAP_PARCELLS_X; x++, nibblerMapParcelNbr++) {
             addNibblerParcel(nibblerMap,
                 Arcade::Vector3f(NIBBLER_PADDING_WINDOW_X + x * PARCELL_SIZE,
