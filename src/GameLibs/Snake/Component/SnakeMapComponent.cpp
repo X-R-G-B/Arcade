7c594e9d9e07ce16d582aebf154eb52e2041234f
Xavier Mitault <xavier.mitault@epitech.eu>
2023-04-05 17:04:41 +0200

    Set bigger size defaultchar

--- src/GameLibs/Snake/Component/SnakeMapComponent.cpp
+++ src/GameLibs/Snake/Component/SnakeMapComponent.cpp
@@ -8,7 +8,6 @@
 #include "GraphStruct.hpp"
 #include "SnakeMapComponent.hpp"
 
-static const std::string defaultChar = "~~~\n~~~";
 static const Arcade::Graph::Color foregroundColorDark(0, 180, 0, 0);
 static const Arcade::Graph::Color backgroundColorDark(0, 180, 0, 0);
 static const Arcade::Graph::Color foregroundColorBright(0, 160, 0, 0);
@@ -16,7 +15,7 @@ static const Arcade::Graph::Color backgroundColorBright(0, 160, 0, 0);
 
 #define DARK_PARCELL "assets/snake/normal/bg_green_01.png"
 #define LIGHT_PARCELL "assets/snake/normal/bg_green_02.png"
-#define DEFAULT_CHAR "~~\n~~"
+#define DEFAULT_CHAR "~~~~\n~~~~"
 
 Snake::Component::SnakeMapComponent::SnakeMapComponent(const std::string &id, const Arcade::Vector3f &pos, bool darkColor)
     : Arcade::Graph::Sprite(id)
