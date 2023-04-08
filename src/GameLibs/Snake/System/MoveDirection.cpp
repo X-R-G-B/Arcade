d9ec705b1ebbbe315f0b68dffc3445f7fe175d49
romain <romain.panno@epitech.eu>
2023-04-08 17:58:08 +0200

    Fix missing check in score for empty score added to ifstream

--- src/GameLibs/Snake/System/MoveDirection.cpp
+++ src/GameLibs/Snake/System/MoveDirection.cpp
@@ -44,29 +44,69 @@ bool Snake::System::MoveDirection::checkHitChangeDir(
     std::shared_ptr<Arcade::ECS::IEntity> entity,
     Arcade::ECS::IComponent &bodyGrow)
 {
+    std::puts("checkHitChangeDir");
     auto &bodySpriteComps = entity->getComponents(Arcade::ECS::CompType::SPRITE);
+    std::puts("bodyGrowComp");
     auto &bodyGrowComp = static_cast<Snake::Component::SnakeGrow &>(bodyGrow);
+    std::puts("hit");
     bool hit = false;
 
+    std::puts("for");
     for (auto &bodySpriteComp : bodySpriteComps) {
+        std::puts("bodySprite");
         auto bodySprite = std::static_pointer_cast<Arcade::Graph::Sprite>(bodySpriteComp);
-        if (bodySprite->pos.x + PADDING_SIZE < changeDir->pos.x ||
-            bodySprite->pos.x + PARCELL_SIZE - (PADDING_SIZE) > changeDir->pos.x + PARCELL_SIZE ||
-            bodySprite->pos.y + PADDING_SIZE < changeDir->pos.y ||
-            bodySprite->pos.y + PARCELL_SIZE - PADDING_SIZE > changeDir->pos.y + PARCELL_SIZE
-        ) {
+        std::printf("bodySprite->pos.x: %p\n", bodySprite->pos.x);
+        std::puts("if");
+//        if (bodySprite->pos.x + PADDING_SIZE < changeDir->pos.x ||
+//            bodySprite->pos.x + PARCELL_SIZE - (PADDING_SIZE) > changeDir->pos.x + PARCELL_SIZE ||
+//            bodySprite->pos.y + PADDING_SIZE < changeDir->pos.y ||
+//            bodySprite->pos.y + PARCELL_SIZE - (PADDING_SIZE) > changeDir->pos.y + PARCELL_SIZE
+//        ) {
+        std::puts("continue1");
+        if (bodySprite->pos.x) {
+            std::puts("continue1.1");
+        }
+        std::puts("continue1.2");
+        if (bodySprite->pos.x + PADDING_SIZE) {
+            std::puts("continue1.2");
+        }
+        std::puts("continue1.3");
+        if (changeDir->pos.x) {
+            std::puts("continue1.3");
+        }
+        std::puts("continue1.4");
+        if (bodySprite->pos.x + PADDING_SIZE < changeDir->pos.x) {
             continue;
         }
+        std::puts("continue2");
+        if (bodySprite->pos.x + PARCELL_SIZE - (PADDING_SIZE) > changeDir->pos.x + PARCELL_SIZE) {
+            continue;
+        }
+        std::puts("continue3");
+        if (bodySprite->pos.y + PADDING_SIZE < changeDir->pos.y) {
+            continue;
+        }
+        std::puts("continue4");
+        if (bodySprite->pos.y + PARCELL_SIZE - (PADDING_SIZE) > changeDir->pos.y + PARCELL_SIZE) {
+            continue;
+        }
+
+        std::puts("bodyDir");
         auto &bodyDir = static_cast<Snake::Component::Forward &>(entity->getComponents(FORWARD_KEY));
         // auto nextCase = Snake::System::MoveInput::toNextCase(bodySprite->pos, bodyDir.direction);
         // bodySprite->pos.x = nextCase.x;
         // bodySprite->pos.y = nextCase.y;
+        std::puts("bodyDir.direction = changeDir->direction;");
         bodyDir.direction = changeDir->direction;
+        std::puts("if");
         if (entity->getId() == bodyGrowComp.lastIdBodyComp) {
+            std::puts("hit = true;");
             hit = true;
             break;
         }
+        std::puts("break");
     }
+    std::puts("return");
     return hit;
 }
 
